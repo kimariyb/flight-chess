c7a84be173202563967881e289b860297350b896
kimariyb <kimariyb@163.com>
2023-08-15 17:07:07 +0800

    update the first line

--- Flight Chess/Flight Chess/Program.cs
+++ Flight Chess/Flight Chess/Program.cs
@@ -4,12 +4,17 @@ namespace Flight_Chess
 {
     internal class Program
     {
-        static int[] Maps = new int[100];
+        // 声明静态字段来模拟全局变量
+        static int[] _map = new int[100];
+
+        // 声明一个静态数组存储玩家 A 和玩家 B 的坐标
+        static int[] _playerPos = new int[2];
 
         public static void Main(string[] args)
         {
             GameShow();
             InitailMap();
+            DrawMap();
             Console.ReadKey();
         }
 
@@ -19,22 +24,22 @@ namespace Flight_Chess
         /// </summary>
         static void GameShow()
         {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("====================================");
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("====================================");
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("====================================");
+            Console.WriteLine("\t============================================");
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("========= Welcome to play ==========");
+            Console.WriteLine("\t============================================");
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("====================================");
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("====================================");
+            Console.WriteLine("\t============================================");
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("====================================");
+            Console.WriteLine("\t============= Welcome to play ==============");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\t============================================");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\t============================================");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\t============================================");
         }
-        
+
         /// <summary>
         /// 初始化地图
         /// </summary>
@@ -45,28 +50,85 @@ namespace Flight_Chess
             for (int i = 0; i < luckyTurn.Length; i++)
             {
                 // 将 Maps 中符合 LuckyTurn 赋值为 1
-                Maps[luckyTurn[i]] = 1;
+                _map[luckyTurn[i]] = 1;
             }
+
             // 地雷
             int[] landMine = { 5, 13, 17, 33, 38, 50, 64, 80, 94 };
             for (int i = 0; i < landMine.Length; i++)
             {
                 // 将 Maps 中符合 landMine 赋值为 2
-                Maps[landMine[i]] = 2;
+                _map[landMine[i]] = 2;
             }
+
             // 暂停
             int[] pause = { 9, 27, 60, 93 };
             for (int i = 0; i < pause.Length; i++)
             {
                 // 将 Maps 中符合 pause 赋值为 3
-                Maps[pause[i]] = 3;
+                _map[pause[i]] = 3;
             }
+
             // 时空隧道
             int[] timeTurn = { 20, 25, 45, 63, 72, 88, 90 };
             for (int i = 0; i < timeTurn.Length; i++)
             {
                 // 将 Maps 中符合 timeTurn 赋值为 4
-                Maps[timeTurn[i]] = 4;
+                _map[timeTurn[i]] = 4;
+            }
+        }
+
+        /// <summary>
+        /// 画地图
+        /// </summary>
+        static void DrawMap()
+        {
+            // 第一行
+            for (int i = 0; i < 30; i++)
+            {
+                // 如果玩家 A 和玩家 B 的坐标相同，画一个符号 <>
+                if (_playerPos[0] == _playerPos[1] && _playerPos[1] == i)
+                {
+                    Console.Write("<>");
+                }
+                else if (_playerPos[0] == i)
+                {
+                    // 全角　Ａ　Ｂ
+                    Console.Write("Ａ");
+                }
+                else if (_playerPos[1] == i)
+                {
+                    // 全角　Ａ　Ｂ
+                    Console.Write("Ｂ");
+                }
+                else
+                {
+                    switch (_map[i])
+                    {
+                        case 0:
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write("□");
+                            break;
+                        case 1: 
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("◎");
+                            break;
+                        case 2: 
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("☆");
+                            break;
+                        case 3: 
+                            Console.ForegroundColor = ConsoleColor.Blue;
+
+                            Console.Write("▲");
+                            break;
+                        case 4: 
+                            Console.ForegroundColor = ConsoleColor.DarkCyan;
+                            Console.Write("※");
+                            break;
+                    }
+                }
+
             }
         }
     }
